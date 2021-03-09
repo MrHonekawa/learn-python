46e806c492ad2e8750edcc8c8a5b75a954d5ab11
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-03-09 08:51:56 +0530

    Update add.py

--- Addition/add.py
+++ Addition/add.py
@@ -1,1 +1,15 @@
+# Today you will learn about..
+# How to code 'Addition_of_two_numbers'
 
+print('Coded by: @EdwinMcCoy')
+a = int(input('Enter the number: ))
+b = int(input('Enter the number: ))
+c = a + b
+print'Sum of both the number is: ',c
+              
+# Code Done
+# Start Python Shell
+# Save this file
+# Run this file
+# Enter the number
+# You'll get answer!
